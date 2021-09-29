838c4c04a820af9dce5c4446dae18a496f8f9667
Evan Rosson <git@erosson.org>
2021-09-29 16:03:23 -0400

    arg parsing

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,37 @@
 import * as Parser from './parser'
 import Elm from './codegen/elm'
+import Minimist from 'minimist'
 
+const usage = `usage: \`fluent-typesafe --format=[elm|ts] FTL_PATH\``
+type Format = 'elm' | 'ts'
+async function run(format: Format, r: Parser.Resource): Promise<string> {
+    switch (format) {
+        case 'elm': return Elm(r.messages)
+        case 'ts': throw new Error('TODO')
+    }
+}
+function parseFormat(args: Minimist.ParsedArgs) {
+    switch (args.format) {
+        case 'elm': return 'elm'
+        case 'ts': return 'ts'
+        default: throw new Error(usage)
+    }
+}
 async function main() {
-    // const paths = ['ftl/localization/en-US/main.ftl']
-    const paths = [process.argv[2]]
+    const args = Minimist(process.argv.slice(2), { string: ['format'] })
+    const paths: string[] = [args._[0]]
+    const format: Format = parseFormat(args)
+    // console.error(args, paths, format)
     const resources: Parser.Resource[] = await Promise.all(paths.map(Parser.parseResource))
-    // console.log(resources)
+    // console.error(resources)
     resources.flat().map(async r => {
         // r.messages.map(m => console.log(m))
-        const out: string = await Elm(r.messages)
+        const out: string = await run(format, r)
         // console.log(out)
         process.stdout.write(out)
     })
 }
-main()
+main().catch(e => {
+    console.error(e)
+    process.exit(1)
+})
