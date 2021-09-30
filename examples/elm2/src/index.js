148c0bde6071ce513f4d28b176b6e79ec7d86229
Evan Rosson <git@erosson.org>
2021-09-29 22:36:15 -0400

    move examples to https://github.com/erosson/fluent-typesafe-examples

--- examples/elm2/src/index.js
+++
@@ -1,9 +0,0 @@
-import 'regenerator-runtime' // required for @fluent/web
-import '@fluent/web'
-
-import './main.css'
-import { Elm } from './Main.elm'
-
-Elm.Main.init({
-  node: document.getElementById('root')
-});
--- tests/examples.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-(cd examples/elm2 && yarn test)
